4f63210d35e4a267ad95b346c3f3c041a439dff4
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 21:09:44 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,16 @@ def start(update, context):
 	# Enviar un mensaje a un ID determinado.
 	context.bot.send_message(update.message.chat_id, "**Hi Sir Use My Command /cmds**")
 
+	coin(update, context)
+def coin(update, context):
+	''' ⚪️/⚫️ Moneda 
+	Genera un número elatorio entre 1 y 2.
+	'''
+	cid=update.message.chat_id
+	msg="⚫️ Cara" if random.randint(1,2)==1 else "⚪️ Cruz"
+	# Responde directametne en el canal donde se le ha hablado.
+	update.message.reply_text(msg)
+	
 def main():
 	TOKEN="2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk"
 	updater=Updater(TOKEN, use_context=True)
@@ -14,7 +24,7 @@ def main():
 
 	# Eventos que activarán nuestro bot.
 	dp.add_handler(CommandHandler('start',	start))
-
+dp.add_handler(CommandHandler('coin',	coin))
 	# Comienza el bot
 	updater.start_polling()
 	# Lo deja a la escucha. Evita que se detenga.
