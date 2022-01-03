e22b5ac668af7e4d46dccfdd3e5f4c0317b51ca8
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 21:00:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-updater = Updater('YOUR TOKEN HERE')
+updater = Updater('2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
