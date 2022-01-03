2d9a30e62f002b3e74c84db46481d0e00640871a
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 21:49:16 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,30 @@
-
 import random
 from telegram import (ParseMode)
 from telegram.ext import (Updater, CommandHandler)
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
+def error_callback(update, context):
+logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def start(update, context):
-	''' START '''
-	context.bot.send_message(update.message.chat_id, "<b>Hi Sir Use My Command /cmds</b>", parse_mode=ParseMode.HTML)
+context.bot.send_message(update.message.chat_id, "<b>Hi Sir Use My Command /cmds</b>", parse_mode=ParseMode.HTML)
 
 
 def cmds(update, context):
-	''' coin '''
-	context.bot.send_message(update.message.chat_id, "<b>My Commands Availables: /gen</b>", parse_mode=ParseMode.HTML)
+context.bot.send_message(update.message.reply_text., "<b>My Commands Availables: /gen</b>", parse_mode=ParseMode.HTML)
 
 def main():
 	TOKEN="2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk"
 	updater=Updater(TOKEN, use_context=True)
 	dp=updater.dispatcher
 
-	# Eventos que activarán nuestro bot.
-	# /comandos
 	dp.add_handler(CommandHandler('start',	start))
 	dp.add_handler(CommandHandler('cmds',	cmds))
-
 	dp.add_error_handler(error_callback)
-    # Comienza el bot
 	updater.start_polling()
-    # Lo deja a la escucha. Evita que se detenga.
 	updater.idle()
 
 if __name__ == '__main__':
