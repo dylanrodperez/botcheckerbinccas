737fbdc0b81e0b289378d521d5872ef97ee9fff4
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 20:05:18 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,55 @@
-import os
-import telegram
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater
+from telegram.ext import  CommandHandler, MessageHandler, Filters
+import  os
+import json
 
+#telegram token
+TOKEN = os.environ.get("TOKEN")
 
-def handle_start(update, context):
+#commandhandler for start command
+def start(update, context):
+    yourname = update.message.chat.first_name
 
-    update.message.reply_text('Hi')
+    msg = "Hi "+yourname+"! Welcome to mimic bot."
+    context.bot.send_message(update.message.chat.id, msg)
 
+#Message handler for texts only
+def mimic(update, context):
+    context.bot.send_message(update.message.chat.id, update.message.text)
 
-if __name__ == '__main__':
+    
+#commandhandler for details command
+def details(update, context):
+    context.bot.send_message(update.message.chat.id, str(update))
 
-    token = os.environ['TOKEN']
+#Error handler
+def error(update, context):
+    context.bot.send_message(update.message.chat.id, "Oops! Error encountered!")
 
-    bot = telegram.Bot(token=token)
-
-    updater = Updater(token=token, use_context=True)
-
+#main logic
+def main():
+    
+    #to get the updates from bot
+    updater = Updater(token=TOKEN, use_context=True)
+    
+    #to dispatch the updates to respective handlers
     dp = updater.dispatcher
-    dp.add_handler(
-        CommandHandler('start', handle_start)
-    )
+    
+    #handlers
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("details", details))
 
-    updater.start_polling()
+    dp.add_handler(MessageHandler(Filters.text, mimic))
 
-    print(f'running at @{bot.username}')
 
+    dp.add_error_handler(error)
+    
+    #to start webhook
+    updater.start_webhook(listen="0.0.0.0",port=os.environ.get("PORT",443),
+                          url_path=TOKEN,
+                          webhook_url="https://mimic-app.herokuapp.com/"+TOKEN)
     updater.idle()
+
+#start application with main function
+if __name__ == '__main__':
+    main()
