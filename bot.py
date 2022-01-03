bd94e26e58516791e27a6000bc4424f13dc1256a
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 20:11:16 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,43 @@
-from telegram.ext import Updater
-from telegram.ext import  CommandHandler, MessageHandler, Filters
-import  os
-import json
+import os
+import telegram
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler
 
-#telegram token
-TOKEN = os.environ['TOKEN']
 
-#commandhandler for start command
-def start(update, context):
+def handle_start(update, context):
+
+    update.message.reply_text(
+        text=(
+            'This bot has been migrated to a new one: @ForwarderGeniusBot.'
+            '\nGo there and run /start to continue'
+        ),
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Go to the bot', url='https://t.me/ForwarderGeniusBot')]
+        ])
+    )
+
+    def start(update, context):
     yourname = update.message.chat.first_name
 
     msg = "Hi "+yourname+"! Welcome to mimic bot."
     context.bot.send_message(update.message.chat.id, msg)
 
-#Message handler for texts only
-def mimic(update, context):
-    context.bot.send_message(update.message.chat.id, update.message.text)
 
-    
-#commandhandler for details command
-def details(update, context):
-    context.bot.send_message(update.message.chat.id, str(update))
-
-#Error handler
-def error(update, context):
-    context.bot.send_message(update.message.chat.id, "Oops! Error encountered!")
-
-#main logic
-def main():
-    
-    #to get the updates from bot
-    updater = Updater(token=TOKEN, use_context=True)
-    
-    #to dispatch the updates to respective handlers
-    dp = updater.dispatcher
-    
-    #handlers
-    dp.add_handler(CommandHandler("start",start))
-    dp.add_handler(CommandHandler("details", details))
-
-    dp.add_handler(MessageHandler(Filters.text, mimic))
-
-
-    dp.add_error_handler(error)
-    
-    #to start webhook
-    updater.start_webhook(listen="0.0.0.0",port=os.environ.get("PORT",443),
-                          url_path=TOKEN,
-                          webhook_url="https://mimic-app.herokuapp.com/"+TOKEN)
-    updater.idle()
-
-#start application with main function
 if __name__ == '__main__':
-    main()
+
+    token = os.environ['TOKEN']
+
+    bot = telegram.Bot(token=token)
+
+    updater = Updater(token=token, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(
+        CommandHandler('start', start)
+    )
+
+    updater.start_polling()
+
+    print(f'running at @{bot.username}')
+
+    updater.idle()
