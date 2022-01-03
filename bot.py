bd326dde586cdf17a3f2f6ba0d21191f27b6a744
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 22:08:30 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ updater=Updater(TOKEN, use_context=True)
 dp=updater.dispatcher
 
 dp.add_handler(CommandHandler('start',	start))
-dp.add_handler(CommandHandler('cmds',	coin))
+dp.add_handler(CommandHandler('coin',	coin))
 
 dp.add_error_handler(error_callback)
 updater.start_polling()
