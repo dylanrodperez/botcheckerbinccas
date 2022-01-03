fff887309bfc88a52f2f24b87aaa4397cb7e6b6f
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 22:50:45 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,35 +2,45 @@ import random
 from telegram import (ParseMode)
 from telegram.ext import (Updater, CommandHandler)
 
+# [Opcional] Recomendable poner un log con los errores que apareceran por pantalla.
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 logger = logging.getLogger(__name__)
 def error_callback(update, context):
-logger.warning('Update "%s" caused error "%s"', update, context.error)
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def start(update, context):
 	''' START '''
-context.bot.send_message(update.message.chat_id, "Bienvenido", parse_mode=ParseMode.HTML)
+	# Enviar un mensaje a un ID determinado.
+	context.bot.send_message(update.message.chat_id, "Bienvenido", parse_mode=ParseMode.HTML)
 
-def coin(update, context):
+	# Podemos llamar a otros comandos, sin que se haya activado en el chat (/help).
+#	coin(update, context)
+def cmds(update, context):
 	''' ⚪️/⚫️ Moneda 
 	Genera un número elatorio entre 1 y 2.
 	'''
-cid=update.message.chat_id
-msg="⚫️ Cara" if random.randint(1,2)==1 else "⚪️ Cruz"
-update.message.reply_text(msg)
+	cid=update.message.chat_id
+	msg="MY Commands: <code>/gen</code>"
+	# Responde directametne en el canal donde se le ha hablado.
+	update.message.reply_text(msg)
 
 def main():
-TOKEN="2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk"
-updater=Updater(TOKEN, use_context=True)
-dp=updater.dispatcher
+	TOKEN="2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk"
+	updater=Updater(TOKEN, use_context=True)
+	dp=updater.dispatcher
+	# Eventos que activarán nuestro bot.
+	# /comandos
 	dp.add_handler(CommandHandler('start',	start))
-	dp.add_handler(CommandHandler('coin',	coin))
+	dp.add_handler(CommandHandler('cmds',	cmds))
 
-dp.add_error_handler(error_callback)
-updater.start_polling()
-updater.idle()
+	dp.add_error_handler(error_callback)
+    # Comienza el bot
+	updater.start_polling()
+    # Lo deja a la escucha. Evita que se detenga.
+	updater.idle()
 
 if __name__ == '__main__':
-print(('[Nombre del bot] Start...'))
-main()
+	print(('[Nombre del bot] Start...'))
+	main()
