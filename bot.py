7d67cddeaaff10f37ad51757361e168a502fc418
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-03 00:38:58 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,61 @@
-import random
-from telegram import (ParseMode)
-from telegram.ext import (Updater, CommandHandler)
-
-# [Opcional] Recomendable poner un log con los errores que apareceran por pantalla.
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enables logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 logger = logging.getLogger(__name__)
-def error_callback(update, context):
+
+PORT = int(os.environ.get('PORT', '8443'))
+
+# We define command handlers. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Sends a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Sends a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echos the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Logs Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def start(update, context):
-    cid=update.message.chat_id
-    msg="<b>Hi Sir Send /cmds For Watch My Commands Availabes</b>"
-    # Responde directametne en el canal donde se le ha hablado.
-    update.message.reply_text(msg, parse_mode=ParseMode.HTML)
-
-    # Podemos llamar a otros comandos, sin que se haya activado en el chat (/help).
-#   coin(update, context)
-def cmds(update, context):
-    cid=update.message.chat_id
-    msg="<b>My Commands Availables: <code>/gen</code></b>"
-    # Responde directametne en el canal donde se le ha hablado.
-    update.message.reply_text(msg, parse_mode=ParseMode.HTML)
-
-    
 def main():
-    TOKEN="2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk"
-    updater=Updater(TOKEN, use_context=True)
-    dp=updater.dispatcher
-    # Eventos que activarán nuestro bot.
-    # /comandos
-    dp.add_handler(CommandHandler('start',  start))
-    dp.add_handler(CommandHandler('cmds',   cmds))
+    """Starts the bot."""
+    # Creates the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    TOKEN = '2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk'#enter your token here
+    APP_NAME='https://pythonbotdr.herokuapp.com/'#Edit the heroku app-name
+    
+    updater = Updater(TOKEN, use_context=True)
 
-    dp.add_error_handler(error_callback)
-    # Comienza el bot
-    updater.start_polling()
-    # Lo deja a la escucha. Evita que se detenga.
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, webhook_url=APP_NAME + TOKEN)
     updater.idle()
 
+
 if __name__ == '__main__':
-    print(('[Nombre del bot] Start...'))
     main()
