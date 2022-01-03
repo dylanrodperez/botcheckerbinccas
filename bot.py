5a56922e0f56f91109bb5c6af07a17d14d5c7a66
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 21:11:54 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
-
+'''
+Bot para telegram
+'''
+import random
+from telegram import (ParseMode)
 from telegram.ext import (Updater, CommandHandler)
+
+# [Opcional] Recomendable poner un log con los errores que apareceran por pantalla.
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
+def error_callback(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def start(update, context):
 	''' START '''
 	# Enviar un mensaje a un ID determinado.
-	context.bot.send_message(update.message.chat_id, "**Hi Sir Use My Command /cmds**")
+	context.bot.send_message(update.message.chat_id, "Bienvenido", parse_mode=ParseMode.HTML)
 
+	# Podemos llamar a otros comandos, sin que se haya activado en el chat (/help).
 	coin(update, context)
 def coin(update, context):
 	''' ⚪️/⚫️ Moneda 
@@ -16,19 +28,23 @@ def coin(update, context):
 	msg="⚫️ Cara" if random.randint(1,2)==1 else "⚪️ Cruz"
 	# Responde directametne en el canal donde se le ha hablado.
 	update.message.reply_text(msg)
-	
+
 def main():
-	TOKEN="2073976428:AAHyWgKV5CQ6f_n6vJy3zAao_Ujy4QDoMAk"
+	TOKEN="123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
 	updater=Updater(TOKEN, use_context=True)
 	dp=updater.dispatcher
 
 	# Eventos que activarán nuestro bot.
+	# /comandos
 	dp.add_handler(CommandHandler('start',	start))
-dp.add_handler(CommandHandler('coin',	coin))
-	# Comienza el bot
+	dp.add_handler(CommandHandler('coin',	coin))
+
+	dp.add_error_handler(error_callback)
+    # Comienza el bot
 	updater.start_polling()
-	# Lo deja a la escucha. Evita que se detenga.
+    # Lo deja a la escucha. Evita que se detenga.
 	updater.idle()
 
 if __name__ == '__main__':
+	print(('[Nombre del bot] Start...'))
 	main()
