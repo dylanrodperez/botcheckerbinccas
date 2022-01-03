9ddba9e2f440954389dc5100fb678c6f8ebe8ea3
dylanrodperez <97002981+dylanrodperez@users.noreply.github.com>
2022-01-02 21:01:48 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import Update
+import Updater, CommandHandler, CallbackContext
 
 
 def hello(update: Update, context: CallbackContext) -> None:
